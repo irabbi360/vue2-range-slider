173796a1a17128736fc8c9d273c85c92dd02726a
Fazle Rabbi <35403788+irabbi360@users.noreply.github.com>
2023-04-12 23:33:16 +0600

    package update & removed unused code files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld :min="500" :max="50000" v-model="value"/>
   </div>
 </template>
@@ -25,12 +24,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
