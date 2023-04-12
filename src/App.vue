ebe75f4ff6629e4af352b27f579afc12d1c95df2
Fazle Rabbi <fazle08rabbi@gmail.com>
2023-04-12 15:17:52 +0600

    package json updated

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :min="500" :max="50000" v-model="value"/>
   </div>
 </template>
 
@@ -12,7 +12,15 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+      return {
+        value: {
+          min: 0,
+          max: 0
+        }
+      }
+    }
 }
 </script>
 
